6792f3558ae4d8682e0e06fdc87b467bbfa9c52a
Кирилл Парыгин <parygin.ks@gmail.com>
2023-10-15 00:30:45 +0500

    Добавьте файлы проекта.

---
+++ Zadacha_01.cs
@@ -0,0 +1,26 @@
+﻿namespace Задача_01_02_03
+{
+
+   internal class Zadacha_01
+    {
+        public void Kvadrat(string? a, int b)
+        {
+            for (int i = 0; i < b; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    Console.Write(b);
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Укажите из какого символа рисовать квадрат: ");
+            string? symb=Console.ReadLine();
+            Console.Write("Укажите длину грани квадрата: ");
+            int dlina = Convert.ToInt32(Console.ReadLine());
+            Kvadrat(symb, dlina);           
+        }
+   }
+}
---
+++ Zadacha_02.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_01_02_03
+{
+    internal class Zadacha_02
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            string? number = Console.ReadLine();
+
+            void CheckingNumber(string number)
+            {
+                if (number[0] == number[3] || number[1] == number[2])
+                {
+                    Console.WriteLine($"Ваше число: {number} - палиндром.");
+                }
+                else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+            }
+
+            if (number!.Length < 5)
+            {
+                CheckingNumber(number);
+            }
+            else Console.WriteLine($"Введи правильное число");
+        }
+    }
+}
